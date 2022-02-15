2fb8f6592d454e148fe547763360184b1d0ee24a
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2022-02-15 16:26:30 +0400

    Update transaction gas fees actions.js
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- app/scripts/controllers/transactions/index.js
+++ app/scripts/controllers/transactions/index.js
@@ -472,7 +472,7 @@ export default class TransactionController extends EventEmitter {
         gasLimit,
         gasPrice,
         maxPriorityFeePerGas,
-        maxFeePerGas,
+        maxFeePerGas,      
       },
       estimateUsed,
       estimateSuggested,
