96b8334a76c30fe2a0c82346e57ae12c6dca0782
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2022-02-17 22:31:31 +0400

    Fix swapApprovalTransaction update to use same return method as swap update Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/store/actions.js
+++ ui/store/actions.js
@@ -730,7 +730,9 @@ export function updateSwapTransaction(txId, txSwap) {
       dispatch(updateMetamaskState(newState));
       dispatch(showConfTxPage({ id: txSwap.id }));
       return txSwap;
-    } catch(err) {}   
+    } finally {
+      dispatch(hideLoadingIndication());
+    }
   };
 }
 
