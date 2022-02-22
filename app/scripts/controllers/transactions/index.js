0333e59a71dd7dbf29f7e1b8d27f5bb9c82582cf
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2022-02-23 00:29:07 +0400

    Add more params to updateTransactionGasFees.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- app/scripts/controllers/transactions/index.js
+++ app/scripts/controllers/transactions/index.js
@@ -505,6 +505,8 @@ export default class TransactionController extends EventEmitter {
         gasPrice,
         maxPriorityFeePerGas,
         maxFeePerGas,
+        estimateUsed,
+        estimateSuggested,
       },
       estimateUsed,
       estimateSuggested,
