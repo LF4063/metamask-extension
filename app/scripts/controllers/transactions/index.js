e1d5a82fc28bee00e40ac5766ef77c7da4af34da
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2022-02-23 22:14:57 +0400

    A few more tests to cover if     transaction status is not unapproved     transaction is passed more parameters than it requires.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- app/scripts/controllers/transactions/index.js
+++ app/scripts/controllers/transactions/index.js
@@ -474,8 +474,6 @@ export default class TransactionController extends EventEmitter {
         gasPrice,
         maxPriorityFeePerGas,
         maxFeePerGas,
-        estimateUsed,
-        estimateSuggested,
       },
       estimateUsed,
       estimateSuggested,
